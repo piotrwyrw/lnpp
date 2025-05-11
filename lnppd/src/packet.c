1f2f5609ddefb8e321476fab26a840fbcc39e30e
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2025-05-11 19:08:24 +0200

    Initialize packet memory with 0s

--- lnppd/src/packet.c
+++ lnppd/src/packet.c
@@ -2,14 +2,20 @@
 
 #include <string.h>
 
+static void _lnpp_packet_prime(struct lnpp_packet *dst, enum lnpp_pktype type)
+{
+	memset(dst, 0, sizeof(struct lnpp_packet));
+	dst->type = type;
+}
+
 void lnpp_packet_advert(struct lnpp_packet *dst, char *name)
 {
-	dst->type = LNPP_ADVERTISEMENT;
+	_lnpp_packet_prime(dst, LNPP_ADVERTISEMENT);
 	strlcpy(dst->advert.name, name, PKT_ADV_LEN - 1);
 }
 
 void lnpp_packet_ping(struct lnpp_packet *dst, time_t time)
 {
-	dst->type = LNPP_PING;
+	_lnpp_packet_prime(dst, LNPP_PING);
 	dst->ping.time = time;
 }
